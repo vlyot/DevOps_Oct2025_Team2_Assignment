c78fcd45e75b12321f71d7c0474f044b3b876323
Zhan Yang <148023902+LiewZhanYang@users.noreply.github.com>
2026-02-11 20:54:44 +0800

    feat(unit-testing): create user form frontend test

--- services/frontend/vite.config.ts
+++ services/frontend/vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 5173,
-    }
+  plugins: [react()],
+  server: {
+    port: 5173,
+  },
+  test: {
+    globals: true, // <--- This fixes "describe is not defined"
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts", // <--- This runs your setup file
+    css: true,
+  },
 });
