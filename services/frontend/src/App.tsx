7eb080aa1ab7e96ad4a3f2da1ca2a7e176089387
Zhan Yang <148023902+LiewZhanYang@users.noreply.github.com>
2026-02-12 14:31:49 +0800

    fix(frontend): restructure the frontend codes

--- services/frontend/src/App.tsx
+++ services/frontend/src/App.tsx
@@ -1,21 +1,21 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Login from './pages/Login';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
 import AdminPage from "./pages/AdminPage";
-import { UserDashboard } from './features/user/UserDashboard';
+import { UserDashboard } from "./pages/user/UserDashboard";
 
 function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Navigate to="/login" />} />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
 
-                <Route path="/login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
 
-                <Route path="/admin" element={<AdminPage />} />
-                <Route path="/dashboard" element={<UserDashboard />} />
-            </Routes>
-        </BrowserRouter>
-    );
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/dashboard" element={<UserDashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
